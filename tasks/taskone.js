0674a9956f9323c28dcbe1b5afdb90a6536210c1
thenodechu <nodechu@gmail.com>
2021-02-15 21:42:33 +0300

    first commit

--- tasks/taskone.js
+++ tasks/taskone.js
@@ -10,27 +10,59 @@ class Converter {
             "km": 1000
         }
         this.instructionForConverting = instructionForConverting;
-
+        
 
     }
+    checkUnits(option) {
+        let result = 1;
+        let permittedUnits = ["m", "km", "yd", "in", "cm", "ft", "mm"];
+        for (let item = 0; item < permittedUnits.length; item++) { 
+            if (option !== permittedUnits[item]) {
+                
+                result -= 1;
+            } else if (option === permittedUnits[item]) {
+                result = 1;
+                break;
+            }
+        }
+        return result;
+    }
+
+    checkValue(option) {
+        let result;
+        if (typeof option === "number") {
+            result = 1;
+        } else {
+            result = -4;
+        }
+        return result;
+    }
     convertToValueOfAnotherType(option) {
-        const distanceToConvert = option["distance"]["value"]
-        const valueToConvert = this.instructionForConverting[option["convertTo"]];
-        const valueConvertFrom = this.instructionForConverting[option["distance"]["unit"]];
-        let result = distanceToConvert * valueConvertFrom / valueToConvert;
-        return { "unit": option["convertTo"], "value": result };
+
+        let check = this.checkUnits(option["distance"]["unit"]) + this.checkUnits(option["convertTo"]) + this.checkValue(option["distance"]["value"]);
+
+        if (check === 3) {
+            const distanceToConvert = option["distance"]["value"]
+            const valueToConvert = this.instructionForConverting[option["convertTo"]];
+            const valueConvertFrom = this.instructionForConverting[option["distance"]["unit"]];
+            let result = distanceToConvert * valueConvertFrom / valueToConvert;
+            return { "unit": option["convertTo"], "value": result };
+        } else {
+            console.log("Error")
+        }
+      
     }
 }
 
 let converter = new Converter();
 
-console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "m", "value": 300 }, "convertTo": "km" }))
+console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "m", "value": 3 }, "convertTo": "km" }))
 console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "km", "value": 0.005 }, "convertTo": "m" }))
 console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "m", "value": 0.31 }, "convertTo": "in" }))
 console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "in", "value": 0.5 }, "convertTo": "m" }))
 console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "m", "value": 0.5 }, "convertTo": "yd" }))
 console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "yd", "value": 0.5 }, "convertTo": "m" }))
 
-console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "m", "value": 0.9144 }, "convertTo": "ft" }))
-console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "ft", "value": 0.5 }, "convertTo": "m" }))
-console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "km", "value": 0.0006096 }, "convertTo": "ft" }))
+console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "mdsa", "value": 0.9144 }, "convertTo": "ft" }))
+console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "543ft", "value": "342" }, "convertTo": "m" }))
+console.log(converter.convertToValueOfAnotherType({ "distance": { "unit": "km", "value":"432432" }, "convertTo": "ft" }))
